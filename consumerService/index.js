874bbaae72f6617d8f08054359cfef0a4ee62f52
Sai Yashwanth <taddishetty34@gmail.com>
2024-03-27 20:22:30 +0530

    Add: Added publish function added

--- consumerService/index.js
+++ consumerService/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {});
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- producerService/index.js
+++ producerService/index.js
@@ -1,6 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const amqp = require('amqplib');
+const bodyParser = require('body-parser');
+const RMQ_URL = '...';
+
+app.use(bodyParser.json());
+
+
+async function pub(exchange, routingKey, message) {
+  try{
+    // Todo: Connect to rabbitMQ server and publish message.
+    const con = await amqp.connect(RMQ_URL);
+    const channel = await con.createChannel();
+
+    await channel.assertExchange(exchange, 'topic', { durable: flase });
+
+    await channe;.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)));
+    
+    await channel.close();
+    await con.close();
+
+    console.log(`Message published to exchange "${exchange}" with routing key "${routingKey}"`);
+  } catch(error){
+      console.error(error.message);
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
