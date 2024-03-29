c00394a6daf0ca5c8ba01a034bdf8dd7ebb21c6f
Sai Yashwanth <taddishetty34@gmail.com>
2024-03-29 12:01:12 +0530

    Add: Migration from js to ts

--- producerService/index.ts
+++ producerService/index.ts
@@ -1,13 +1,14 @@
-const express = require('express');
-const app = express();
+import express, {Express, Request, Response} from 'express';
+const app: Express = express();
 const port = 3000;
-const amqp = require('amqplib');
-const bodyParser = require('body-parser');
+import amqp from 'amqplib';
+import bodyParser from 'body-parser';
+
 const RMQ_URL = 'amqp://localhost:5672/';
 
 app.use(bodyParser.json());
 
-async function pub(exchange, routingKey, message) {
+async function pub(exchange: string, routingKey: string, message: any) {
   try{
     // Todo: Connect to rabbitMQ server and publish message.
     console.log("pub function")
@@ -23,16 +24,16 @@ async function pub(exchange, routingKey, message) {
     await con.close();
 
     console.log(`Message published to exchange "${exchange}" with routing key "${routingKey}"`);
-  } catch(error){
+  } catch(error: any){
       console.error("Error in pub function:", error.message);
     throw error;  }
 }
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 });
 
-app.post('/consumer1', async (req, res) => {
+app.post('/consumer1', async (req: Request, res: Response) => {
   try{
     console.log("hi");
     const healthCheckMessage = { "status": "ok" };
@@ -40,43 +41,43 @@ app.post('/consumer1', async (req, res) => {
     await pub("health", '', healthCheckMessage);
 
     res.send('HealthCheck message published successfully');
-  } catch(error){
+  } catch(error: any){
     res.send("error from api");
   }
 });
 
-app.post('/consumer2', async (req, res) => {
+app.post('/consumer2', async (req: Request, res: Response) => {
   try{
     const data = req.body;
     console.log(data);
     await pub('create', '', data);
 
     res.send('Item data published successfully')
-  } catch(error){
+  } catch(error: any){
     res.send(error.message);
   }
 })
 
-app.post('/consumer3', async (req, res) => {
+app.post('/consumer3', async (req: Request, res: Response) => {
   try{
     const data = req.body;
     console.log(data);
     await pub('delete', '', data);
 
     res.send('Item data published successfully')
-  } catch(error){
+  } catch(error: any){
     res.send(error.message);
   }
 })
 
-app.post('/consumer4', async (req, res) => {
+app.post('/consumer4', async (req: Request, res: Response) => {
   try{
     const data = req.body;
     console.log(data);
     await pub('read', '', data);
 
     res.send('Item data published successfully')
-  } catch(error){
+  } catch(error: any){
     res.send(error.message);
   }
 })
@@ -84,3 +85,4 @@ app.post('/consumer4', async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
