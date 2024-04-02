21d600c117735f707eb351971f7d8e1617719d98
Sai Yashwanth <taddishetty34@gmail.com>
2024-04-02 09:25:10 +0530

    Add: Consumer2 base code

---
+++ consumer2/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const amqp = require('amqplib');
+const RMQ_URL = 'amqp://localhost:5672/';
+
+
+async function consumeMessage(exchange, routingKey){
+  try{
+    // Connect to rabbitMQ server and retirieve the message 
+    const con = await amqp.connect(RMQ_URL);
+    const channel = await con.createChannel();
+
+    await channel.assertExchange(exchange, 'topic', { durable: false });
+    
+    const { queue } = await channel.assertQueue('', { exclusive: true });
+
+    await channel.bindQueue(queue, exchange, routingKey);
+
+    console.log(`Waiting for messages from exchange "${exchange}" with routing key "${routingKey}"...`);
+    let m;
+    channel.consume(queue, (msg) => {
+      if (msg) {
+        const messageContent = msg.content.toString();
+        m = msg.content;
+        console.log('Received message:', messageContent);
+        channel.ack(msg);
+      }
+    return m;
+    });
+  } catch(error){
+    console.error('Error: ', error.message);
+  }
+}
+
+app.get('/consumer1', (req, res) => {
+  const msg = consumeMessage('health', '');
+  res.json(msg);
+})
+
+// app.get('/itemcreation', (req, res) => {
+//   consumeMessage('create', '');
+//   res.json("Consumer2 started consuming messages");
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
