a9c9a3475278ef425940abbe4b9f4ecb92593b40
Tannu Bajaj <tbajaj7272@gmail.com>
2024-04-18 13:45:40 +0530

    Add:Consumer four

--- consumer4/index.js
+++ consumer4/index.js
@@ -1,39 +1,69 @@
-const express = require('express')
-const app = express()
-const port = 3004
+const express = require('express');
+const app = express();
+const port = 3004;
 const amqp = require('amqplib');
 const RMQ_URL = 'amqp://localhost:5672/';
+const mysql = require('mysql');
 
+// Setup MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'jacksonwang123$$',
+  database: 'inventory'
+});
 
-async function consumeMessage(exchange, routingKey){
-  try{
-    // Connect to rabbitMQ server and retirieve the message 
-    const con = await amqp.connect(RMQ_URL);
-    const channel = await con.createChannel();
+// Connect to MySQL
+connection.connect(err => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log('Successfully connected to the database');
+});
 
+// Function to handle read operations from the database
+async function handleReadOperation(msg) {
+  const parsedData = JSON.parse(msg);
+  const itemId = parsedData.item_id;
+
+  const sql = 'SELECT * FROM inventory WHERE item_id = ?';
+
+  connection.query(sql, [itemId], (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return;
+    }
+    console.log('Query results:', results);
+  });
+}
+
+// Function to consume messages
+async function consumeMessage(exchange, routingKey) {
+  try {
+    const conn = await amqp.connect(RMQ_URL);
+    const channel = await conn.createChannel();
     await channel.assertExchange(exchange, 'topic', { durable: false });
-    
     const { queue } = await channel.assertQueue('', { exclusive: true });
-
     await channel.bindQueue(queue, exchange, routingKey);
 
     console.log(`Waiting for messages from exchange "${exchange}" with routing key "${routingKey}"...`);
-    channel.consume(queue, (msg) => {
+    channel.consume(queue, msg => {
       if (msg) {
-        const messageContent = msg.content.toString();
-        console.log('Received message:', messageContent);
+        console.log('Received message:', msg.content.toString());
         channel.ack(msg);
+        handleReadOperation(msg.content.toString());
       }
     });
-  } catch(error){
-    console.error('Error: ', error.message);
+  } catch (error) {
+    console.error('Error:', error.message);
   }
 }
 
-
+// Start message consumption
 consumeMessage('read', '');
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Consumer 4 listening on port ${port}`);
+});
